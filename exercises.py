83808f60324dfa39cdb5dfa82fd8a4e7720cf06a
ChristianIshimwe7 <c.ishimwe7@alustudent.com>
2024-02-27 17:55:18 +0200

    My ALU PYTHON exercises

---
+++ exercises.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+print(f"{98} Battery street")
+a = "Python is cool"
+print(a[7:])
+a = "Python is cool"
+print(a[0:6])
+print(f"{98} Battery street, {'San Francisco'}")
+a = "Python is cool"
+print(a[-2])
+a = "Python is cool"
+a = "Python is cool"
+print(a[4])
